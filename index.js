5b09684714145ac8c0395a6eb293cd6142d2e6c1
Allison Howard <alliemhow@gmail.com>
2019-08-28 06:35:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,21 @@
+let store = { drivers: [], passengers: [] };
+
+let driverId = 0
+
+class Driver {
+  constructor(name) {
+    this.name = name
+    this.id = ++driverId
+
+    store.drivers.push(this);
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.name = name
+
+    store.passengers.push(this);.
+  }
+
+}
